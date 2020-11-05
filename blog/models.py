55b94e854a8e2e89f4a8c1768413869a9758cdbf
Gabriel Anguita <gabriel.anguita@magnet.cl>
2020-11-05 12:32:13 -0300

    add bootstrap and jobs show up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    creation_date = models.DateTimeField(default=timezone.now)
+    pub_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def publish(self):
+        self.pub_date = timezone.now()
+        self.save()
